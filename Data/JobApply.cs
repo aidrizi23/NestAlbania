c5defb775847b815782631508024c4358e3a996b
glenisii <ghazizi321@gmail.com>
2024-06-12 12:15:57 +0200

    glenisi job apply

---
+++ Data/JobApply.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NestAlbania.Data
+{
+    public class JobApply
+    {
+        public int Id {  get; set; }
+        
+        [Required]
+        public string Emri { get; set; }
+        [Required]
+        public string NrTel { get; set; }
+        
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Adresa { get; set; }
+
+        [Required]
+        public IFormFile Resume { get; set; }
+
+    }
+}
