bcdfbb683ecea11e7071bb723d05d1466c584aa3
Gersi848 <gersishati22@gmail.com>
2024-06-25 12:36:55 +0200

    user-agent

--- Areas/IUserRepository.cs
+++ Areas/IUserRepository.cs
@@ -11,5 +11,6 @@ namespace NestAlbania.Areas
         Task UpdateUserAsync(ApplicationUser user);
         Task DeleteUserAsync(ApplicationUser user);
         Task SaveChangesAsync();
+        Task<Agent> GetAgentByUserIdAsync(string id);
     }
 }
--- Areas/UserRepository.cs
+++ Areas/UserRepository.cs
@@ -7,46 +7,49 @@ namespace NestAlbania.Areas
 {
     public class UserRepository : IUserRepository
     {
-            private readonly ApplicationDbContext _context;
-            public UserRepository(ApplicationDbContext context)
-            {
-                _context = context;
-            }
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-            public async Task<PaginatedList<ApplicationUser>> GetPaginatedUsersAsync(int page = 1, int pagesize = 10)
-            {
+        public async Task<PaginatedList<ApplicationUser>> GetPaginatedUsersAsync(int page = 1, int pagesize = 10)
+        {
 
-                return await PaginatedList<ApplicationUser>.CreateAsync(_context.ApplicationUsers.AsQueryable(), page, pagesize);
-            }
+            return await PaginatedList<ApplicationUser>.CreateAsync(_context.ApplicationUsers.AsQueryable(), page, pagesize);
+        }
 
-            public async Task<ApplicationUser> GetUserByIdAsync(string id)
-            {
-                return await _context.ApplicationUsers.FindAsync(id);
-            }
+        public async Task<ApplicationUser> GetUserByIdAsync(string id)
+        {
+            return await _context.ApplicationUsers.FindAsync(id);
+        }
 
-            public async Task<IEnumerable<ApplicationUser>> FindAllAsync()
-            {
-                return await _context.ApplicationUsers.ToListAsync();
-            }
+        public async Task<IEnumerable<ApplicationUser>> FindAllAsync()
+        {
+            return await _context.ApplicationUsers.ToListAsync();
+        }
 
-            public async Task UpdateUserAsync(ApplicationUser user)
-            {
-                _context.Update(user);
-                await _context.SaveChangesAsync();
-            }
+        public async Task UpdateUserAsync(ApplicationUser user)
+        {
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+        }
 
-            public async Task DeleteUserAsync(ApplicationUser user)
-            {
-                _context.ApplicationUsers.Remove(user);
-                await _context.SaveChangesAsync();
-            }
+        public async Task DeleteUserAsync(ApplicationUser user)
+        {
+            _context.ApplicationUsers.Remove(user);
+            await _context.SaveChangesAsync();
+        }
 
-            public async Task SaveChangesAsync()
-            {
-                await _context.SaveChangesAsync();
-            }
-
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+        public async Task<Agent> GetAgentByUserIdAsync(string id)
+        {
+            return await _context.Agents.Where(x => x.UserId == id).FirstOrDefaultAsync();
         }
 
     }
+}
 
