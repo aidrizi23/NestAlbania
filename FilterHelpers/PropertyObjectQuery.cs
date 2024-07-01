8c0c9b79f480755ecda3bbf86def5d5a8e7d238e
Albi Idrizi <albiidrizi27@gmail.com>
2024-07-01 09:53:52 +0200

    test albi

--- FilterHelpers/PropertyObjectQuery.cs
+++ FilterHelpers/PropertyObjectQuery.cs
@@ -6,18 +6,14 @@ namespace NestAlbania.FilterHelpers
     public class PropertyObjectQuery
     {
         public string? Name { get; set; } = null;
-        public int? Price { get; set; } = null;
+       
         public int? FullArea { get; set; } = null;
         public int? InsideArea { get; set; } = null;
         public int? BedroomCount { get; set; } = null;
         public int? BathroomCount { get; set; } = null;
-        public string? AgentName { get; set; } = null;
-        public string? Name { get; set; }
         public int? MinPrice { get; set; }
         public int? MaxPrice { get; set; }
-        public int? FullArea { get; set; }
-        public int? InsideArea { get; set; }
-        public int? BedroomCount { get; set; }
-        public int? BathroomCount { get; set; }
+        public string? AgentName { get; set; } = null;
+        
     }
 }
