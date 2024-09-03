997a37096abec0dd1d5a22b2ecff4f45ca480f05
Albi Idrizi <albiidrizi27@gmail.com>
2024-09-03 21:36:59 +0200

    soft deletes

---
+++ Migrations/20240903184603_Soft-delete-property.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NestAlbania.Migrations
+{
+    /// <inheritdoc />
+    public partial class Softdeleteproperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "Properties",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isSold",
+                table: "Properties",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "JobApplications",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "Countries",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "Agents",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "be25118c-5432-4bbd-9fc6-629e4e5fa338", "AQAAAAIAAYagAAAAENQN6LQSUY9zGoT6Lqas16LKEE8vADbP0FQKDtjoRnJo9uUDpOSixMB47QuhSWybCA==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "isSold",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "JobApplications");
+
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "Agents");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "3f95175a-6840-44fb-9e85-e994868a2871", "AQAAAAIAAYagAAAAENv06FnuQPbMG48eHGpy6DXEGg78SehI7Su7myAGaTlSGu9yhWewhBEc1CX0bXpaVA==" });
+        }
+    }
+}
---
+++ Migrations/20240903190853_agent-sold-properties.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NestAlbania.Migrations
+{
+    /// <inheritdoc />
+    public partial class agentsoldproperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AgentId1",
+                table: "Properties",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SoldProperties",
+                table: "Agents",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "0ae49e09-7b9f-4643-9b8f-86d95c33adef", "AQAAAAIAAYagAAAAEPD0OO6MFGLJk6NWyXmPIFOU3P0qG9GalINyXrixF/ebGQPCbvbuynn6cRvcRo0ARA==" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Properties_AgentId1",
+                table: "Properties",
+                column: "AgentId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Properties_Agents_AgentId1",
+                table: "Properties",
+                column: "AgentId1",
+                principalTable: "Agents",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Properties_Agents_AgentId1",
+                table: "Properties");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Properties_AgentId1",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "AgentId1",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "SoldProperties",
+                table: "Agents");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "be25118c-5432-4bbd-9fc6-629e4e5fa338", "AQAAAAIAAYagAAAAENQN6LQSUY9zGoT6Lqas16LKEE8vADbP0FQKDtjoRnJo9uUDpOSixMB47QuhSWybCA==" });
+        }
+    }
+}
