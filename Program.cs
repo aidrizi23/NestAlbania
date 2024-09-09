f1e946c5fb2d09ab9ebc204575a0e6b27fcb10fd
Albi Idrizi <albiidrizi27@gmail.com>
2024-09-09 11:20:55 +0200

    albi lockout options

--- Program.cs
+++ Program.cs
@@ -1,90 +1,4 @@
-// // using Microsoft.AspNetCore.Authentication.Cookies;
-// // using Microsoft.AspNetCore.Authentication.Google;
-// // using Microsoft.AspNetCore.Identity;
-// // using Microsoft.EntityFrameworkCore;
-// // using NestAlbania.Areas;
-// // using NestAlbania.Data;
-// // using NestAlbania.Repositories;
-// // using NestAlbania.Services;
-// // using NestAlbania.Services.Extensions;
-// // using System.Configuration;
-// //
-// // var builder = WebApplication.CreateBuilder(args);
-// //
-// // // Add services to the container.
-// // var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-// // builder.Services.AddDbContext<ApplicationDbContext>(options =>
-// //     options.UseSqlServer(connectionString));
-// // builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-// //
-// // builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
-// //
-// //
-// // builder.Services.AddAuthentication(options =>
-// // {
-// //     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-// //     options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-// // })
-// // .AddCookie()
-// // .AddGoogle(options =>
-// // {
-// //     options.ClientId = "1043913359759-1ce4fe1mr400mjtbqb676e1r931qmu9a.apps.googleusercontent.com";
-// //     options.ClientSecret = "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";
-// // });
-// //
-// // builder.Services.AddControllersWithViews();
-// //
-// // #region Scoped
-// // builder.Services.AddScoped<IUserRepository, UserRepository>();
-// // builder.Services.AddScoped<UserRoleRepository, UserRoleRepository>();
-// // builder.Services.AddScoped<RoleRepository, RoleRepository>();
-// // builder.Services.AddScoped<JobApplyRepository,  JobApplyRepository>();
-// // builder.Services.AddScoped<CountryRepository, CountryRepository>();
-// // builder.Services.AddScoped<PropertyRepository , PropertyRepository>();
-// // builder.Services.AddScoped<AgentRepository, AgentRepository>();
-// // #endregion
-// //
-// // #region Transient
-// // builder.Services.AddTransient<IUserService, UserService>();
-// // builder.Services.AddTransient<IUserRoleService, UserRoleService>();
-// // builder.Services.AddTransient<IRoleService, RoleService>();
-// // builder.Services.AddTransient<IJobApplyService, JobApplyService>();
-// // builder.Services.AddTransient<IPropertyService, PropertyService>();
-// // builder.Services.AddTransient<ICountryService, CountryService>();
-// // builder.Services.AddTransient<IAgentService, AgentService>();
-// // builder.Services.AddTransient<IFileHandlerService, FileHandlerService>();
-// // #endregion
-// //
-// //
-// // var app = builder.Build();
-// //
-// // // Configure the HTTP request pipeline.
-// // if (app.Environment.IsDevelopment())
-// // {
-// //     app.UseMigrationsEndPoint();
-// // }
-// // else
-// // {
-// //     app.UseExceptionHandler("/Home/Error");
-// //     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-// //     app.UseHsts();
-// // }
-// //
-// // app.UseHttpsRedirection();
-// // app.UseStaticFiles();
-// //
-// // app.UseRouting();
-// //
-// // app.UseAuthentication();
-// // app.UseAuthorization();
-// //
-// // app.MapControllerRoute(
-// //     name: "default",
-// //     pattern: "{controller=Account}/{action=Login}/{id?}");
-// //
-// // app.Run();
-//
-//
+
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Identity;
@@ -94,82 +8,6 @@ using NestAlbania.Data;
 using NestAlbania.Repositories;
 using NestAlbania.Services;
 using NestAlbania.Services.Extensions;
-
-// var builder = WebApplication.CreateBuilder(args);
-//
-// // Add services to the container.
-// var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
-//     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-// builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//     options.UseSqlServer(connectionString));
-// builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-//
-// builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options => options.SignIn.RequireConfirmedAccount = true)
-//     .AddEntityFrameworkStores<ApplicationDbContext>()
-//     .AddDefaultTokenProviders();
-//
-// builder.Services.AddAuthentication(options =>
-// {
-//     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-//     options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-// })
-// .AddCookie()
-// .AddGoogle(options =>
-// {
-//     options.ClientId = "1043913359759-1ce4fe1mr400mjtbqb676e1r931qmu9a.apps.googleusercontent.com";
-//     options.ClientSecret = "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";
-// });
-//
-// builder.Services.AddControllersWithViews();
-//
-// #region Scoped
-// builder.Services.AddScoped<IUserRepository, UserRepository>();
-// builder.Services.AddScoped<UserRoleRepository, UserRoleRepository>();
-// builder.Services.AddScoped<RoleRepository, RoleRepository>();
-// builder.Services.AddScoped<JobApplyRepository, JobApplyRepository>();
-// builder.Services.AddScoped<CountryRepository, CountryRepository>();
-// builder.Services.AddScoped<PropertyRepository, PropertyRepository>();
-// builder.Services.AddScoped<AgentRepository, AgentRepository>();
-// #endregion
-//
-// #region Transient
-// builder.Services.AddTransient<IUserService, UserService>();
-// builder.Services.AddTransient<IUserRoleService, UserRoleService>();
-// builder.Services.AddTransient<IRoleService, RoleService>();
-// builder.Services.AddTransient<IJobApplyService, JobApplyService>();
-// builder.Services.AddTransient<IPropertyService, PropertyService>();
-// builder.Services.AddTransient<ICountryService, CountryService>();
-// builder.Services.AddTransient<IAgentService, AgentService>();
-// builder.Services.AddTransient<IFileHandlerService, FileHandlerService>();
-// #endregion
-//
-// var app = builder.Build();
-//
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseMigrationsEndPoint();
-// }
-// else
-// {
-//     app.UseExceptionHandler("/Home/Error");
-//     app.UseHsts();
-// }
-//
-// app.UseHttpsRedirection();
-// app.UseStaticFiles();
-//
-// app.UseRouting();
-//
-// app.UseAuthentication();
-// app.UseAuthorization();
-//
-// // Default route configuration pointing to AccountController's Login action
-// app.MapControllerRoute(
-//     name: "default",
-//     pattern: "{controller=Account}/{action=Login}/{id?}");
-//
-// app.Run();
 
 
 
@@ -185,17 +23,24 @@ builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options => option
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
-// builder.Services.AddAuthentication(options =>
-// {
-//     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-//     options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-// })
-// .AddCookie()
-// .AddGoogle(options =>
-// {
-//     options.ClientId = "1043913359759-1ce4fe1mr400mjtbqb676e1r931qmu9a.apps.googleusercontent.com";
-//     options.ClientSecret = "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";
-// });
+
+// remove all the password requirements
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
+//lockout options
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+});
 
 builder.Services.AddControllersWithViews();
 
